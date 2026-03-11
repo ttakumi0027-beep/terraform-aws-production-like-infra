579d7e6a68d9d634da8bc830f05cc76435105ca7
Tsumuraya Takumi <ttakumi0027@gmail.com>
2026-03-11 19:53:29 +0900

    create subnet all

--- main.tf
+++ main.tf
@@ -29,3 +29,4 @@ variable "project" {
 variable "environment" {
   type = string
 }
+
--- network.tf
+++ network.tf
@@ -15,3 +15,102 @@ resource "aws_vpc" "vpc" {
     Env     = var.environment
   }
 }
+
+# ------------------------
+# Public-Subnet
+# ------------------------
+
+# NAT/ALB Subnet Public-1a
+resource "aws_subnet" "nat_public_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-nat-public-1a"
+    Project = var.project
+    Env     = var.environment
+    Type    = "public"
+  }
+}
+
+# NAT/ALB Subnet Public-1c
+resource "aws_subnet" "nat_public_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-nat-public-1c"
+    Project = var.project
+    Env     = var.environment
+    Type    = "public"
+  }
+}
+
+
+# ------------------------
+# Private-Subnet
+# ------------------------
+
+# App-server Subnet Private-1a
+resource "aws_subnet" "app_private_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-private-1a"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
+
+# App-server Subnet Private-1c
+resource "aws_subnet" "app_private_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-private-1c"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
+
+# DB-server Subnet Private-1a
+resource "aws_subnet" "db_private_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "10.0.4.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-db-private-1a"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
+
+# DB-server Subnet Private-1c
+resource "aws_subnet" "db_private_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "10.0.5.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-db-private-1c"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
