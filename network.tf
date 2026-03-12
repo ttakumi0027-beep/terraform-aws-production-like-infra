0032be82423ff6eb11ee7cd0f688ada8026a2ad7
Tsumuraya Takumi <ttakumi0027@gmail.com>
2026-03-12 15:39:18 +0900

    create igw and route table

--- network.tf
+++ network.tf
@@ -114,3 +114,100 @@ resource "aws_subnet" "db_private_subnet_1c" {
     Type    = "private"
   }
 }
+
+
+# ------------------------
+# Route Table
+# ------------------------
+
+# Public Route Table
+resource "aws_route_table" "nat_public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-nat-public-rt"
+    Project = var.project
+    Env     = var.environment
+    Type    = "public"
+  }
+}
+
+resource "aws_route_table_association" "nat_public_rt_1a" {
+  route_table_id = aws_route_table.nat_public_rt.id
+  subnet_id      = aws_subnet.nat_public_subnet_1a.id
+}
+
+resource "aws_route_table_association" "nat_public_rt_1c" {
+  route_table_id = aws_route_table.nat_public_rt.id
+  subnet_id      = aws_subnet.nat_public_subnet_1c.id
+}
+
+# Private Route Table for App-server
+resource "aws_route_table" "app_private_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-private-rt"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
+
+resource "aws_route_table_association" "app_private_rt_1a" {
+  route_table_id = aws_route_table.app_private_rt.id
+  subnet_id      = aws_subnet.app_private_subnet_1a.id
+}
+
+resource "aws_route_table_association" "app_private_rt_1c" {
+  route_table_id = aws_route_table.app_private_rt.id
+  subnet_id      = aws_subnet.app_private_subnet_1c.id
+}
+
+
+# Private Route Table for DB
+resource "aws_route_table" "db_private_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-db-private-rt"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
+
+resource "aws_route_table_association" "db_private_rt_1a" {
+  route_table_id = aws_route_table.db_private_rt.id
+  subnet_id      = aws_subnet.db_private_subnet_1a.id
+}
+
+resource "aws_route_table_association" "db_private_rt_1c" {
+  route_table_id = aws_route_table.db_private_rt.id
+  subnet_id      = aws_subnet.db_private_subnet_1c.id
+}
+
+
+
+# ------------------------
+# Internet Gateway
+# ------------------------
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.environment}-igw"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_route" "public_rt_igw" {
+  route_table_id         = aws_route_table.nat_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# ------------------------
+# Nat Gateway
+# ------------------------
