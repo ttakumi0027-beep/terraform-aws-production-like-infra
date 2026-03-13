0661d8e7daa59d132d24d4a5e99c57edaa290126
Tsumuraya Takumi <ttakumi0027@gmail.com>
2026-03-13 11:30:03 +0900

    modify network.tf nat-gw

--- network.tf
+++ network.tf
@@ -1,7 +1,7 @@
+
 # ------------------------
 # VPC
 # ------------------------
-
 resource "aws_vpc" "vpc" {
   cidr_block                       = "10.0.0.0/16"
   instance_tenancy                 = "default"
@@ -21,14 +21,14 @@ resource "aws_vpc" "vpc" {
 # ------------------------
 
 # NAT/ALB Subnet Public-1a
-resource "aws_subnet" "nat_public_subnet_1a" {
+resource "aws_subnet" "public_subnet_1a" {
   vpc_id                  = aws_vpc.vpc.id
   availability_zone       = "ap-northeast-1a"
   cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = true
 
   tags = {
-    Name    = "${var.project}-${var.environment}-nat-public-1a"
+    Name    = "${var.project}-${var.environment}-public-1a"
     Project = var.project
     Env     = var.environment
     Type    = "public"
@@ -36,20 +36,19 @@ resource "aws_subnet" "nat_public_subnet_1a" {
 }
 
 # NAT/ALB Subnet Public-1c
-resource "aws_subnet" "nat_public_subnet_1c" {
+resource "aws_subnet" "public_subnet_1c" {
   vpc_id                  = aws_vpc.vpc.id
   availability_zone       = "ap-northeast-1c"
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
 
   tags = {
-    Name    = "${var.project}-${var.environment}-nat-public-1c"
+    Name    = "${var.project}-${var.environment}-public-1c"
     Project = var.project
     Env     = var.environment
     Type    = "public"
   }
 }
-
 
 # ------------------------
 # Private-Subnet
@@ -115,55 +114,77 @@ resource "aws_subnet" "db_private_subnet_1c" {
   }
 }
 
-
 # ------------------------
 # Route Table
 # ------------------------
 
-# Public Route Table
-resource "aws_route_table" "nat_public_rt" {
+# Public-1a Route Table for NAT/ALB
+resource "aws_route_table" "public_rt_1a" {
   vpc_id = aws_vpc.vpc.id
 
   tags = {
-    Name    = "${var.project}-${var.environment}-nat-public-rt"
+    Name    = "${var.project}-${var.environment}-public-1a-rt"
     Project = var.project
     Env     = var.environment
     Type    = "public"
   }
 }
 
-resource "aws_route_table_association" "nat_public_rt_1a" {
-  route_table_id = aws_route_table.nat_public_rt.id
-  subnet_id      = aws_subnet.nat_public_subnet_1a.id
-}
-
-resource "aws_route_table_association" "nat_public_rt_1c" {
-  route_table_id = aws_route_table.nat_public_rt.id
-  subnet_id      = aws_subnet.nat_public_subnet_1c.id
-}
-
-# Private Route Table for App-server
-resource "aws_route_table" "app_private_rt" {
+# Public-1c Route Table for NAT/ALB
+resource "aws_route_table" "public_rt_1c" {
   vpc_id = aws_vpc.vpc.id
 
   tags = {
-    Name    = "${var.project}-${var.environment}-app-private-rt"
+    Name    = "${var.project}-${var.environment}-public-1c-rt"
+    Project = var.project
+    Env     = var.environment
+    Type    = "public"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc_1a" {
+  route_table_id = aws_route_table.public_rt_1a.id
+  subnet_id      = aws_subnet.public_subnet_1a.id
+}
+
+resource "aws_route_table_association" "public_rt_assoc_1c" {
+  route_table_id = aws_route_table.public_rt_1c.id
+  subnet_id      = aws_subnet.public_subnet_1c.id
+}
+
+# App Private Route Table 1a
+resource "aws_route_table" "app_private_rt_1a" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-private-rt-1a"
     Project = var.project
     Env     = var.environment
     Type    = "private"
   }
 }
 
-resource "aws_route_table_association" "app_private_rt_1a" {
-  route_table_id = aws_route_table.app_private_rt.id
+resource "aws_route_table_association" "app_private_rt_assoc_1a" {
+  route_table_id = aws_route_table.app_private_rt_1a.id
   subnet_id      = aws_subnet.app_private_subnet_1a.id
 }
 
-resource "aws_route_table_association" "app_private_rt_1c" {
-  route_table_id = aws_route_table.app_private_rt.id
-  subnet_id      = aws_subnet.app_private_subnet_1c.id
+# App Private Route Table 1c
+resource "aws_route_table" "app_private_rt_1c" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-private-rt-1c"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
 }
 
+resource "aws_route_table_association" "app_private_rt_assoc_1c" {
+  route_table_id = aws_route_table.app_private_rt_1c.id
+  subnet_id      = aws_subnet.app_private_subnet_1c.id
+}
 
 # Private Route Table for DB
 resource "aws_route_table" "db_private_rt" {
@@ -187,14 +208,12 @@ resource "aws_route_table_association" "db_private_rt_1c" {
   subnet_id      = aws_subnet.db_private_subnet_1c.id
 }
 
-
-
 # ------------------------
 # Internet Gateway
 # ------------------------
-
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
+
   tags = {
     Name    = "${var.project}-${var.environment}-igw"
     Project = var.project
@@ -202,8 +221,14 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-resource "aws_route" "public_rt_igw" {
-  route_table_id         = aws_route_table.nat_public_rt.id
+resource "aws_route" "public_rt_igw_1a" {
+  route_table_id         = aws_route_table.public_rt_1a.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route" "public_rt_igw_1c" {
+  route_table_id         = aws_route_table.public_rt_1c.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
@@ -211,3 +236,55 @@ resource "aws_route" "public_rt_igw" {
 # ------------------------
 # Nat Gateway
 # ------------------------
+
+resource "aws_eip" "nat_eip_1a" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project}-${var.environment}-nat-eip-1a"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw_1a" {
+  allocation_id = aws_eip.nat_eip_1a.id
+  subnet_id     = aws_subnet.public_subnet_1a.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-nat-gw-1a"
+    Project = var.project
+    Env     = var.environment
+  }
+
+}
+
+resource "aws_route" "app_private_nat_1a" {
+  route_table_id         = aws_route_table.app_private_rt_1a.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw_1a.id
+}
+
+resource "aws_eip" "nat_eip_1c" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project}-${var.environment}-nat-eip-1c"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw_1c" {
+  allocation_id = aws_eip.nat_eip_1c.id
+  subnet_id     = aws_subnet.public_subnet_1c.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-nat-gw-1c"
+    Project = var.project
+    Env     = var.environment
+  }
+
+}
+
+resource "aws_route" "app_private_nat_1c" {
+  route_table_id         = aws_route_table.app_private_rt_1c.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw_1c.id
+}
