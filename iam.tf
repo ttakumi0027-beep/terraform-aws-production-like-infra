d5e75571ee1c37d8a2cb63fd5b4cc5b15ed1658a
Tsumuraya Takumi <ttakumi0027@gmail.com>
2026-03-18 10:00:05 +0900

    create IAM role for SSm

--- iam.tf
+++ iam.tf
@@ -48,5 +48,54 @@ resource "aws_iam_user_group_membership" "memberships" {
 }
 
 # ------------------------
-# IAM Role
+# EC2 Assume Role Policy
 # ------------------------
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect = "Allow"
+
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+# ------------------------
+# IAM Role for SSM
+# ------------------------
+resource "aws_iam_role" "ec2_ssm_role" {
+  name               = "${var.project}-${var.environment}-ec2-ssm-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-ec2-ssm-role"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+# ------------------------
+# Instance Profile
+# ------------------------
+resource "aws_iam_instance_profile" "ec2_ssm_profile" {
+  name = "${var.project}-${var.environment}-ec2-ssm-profile"
+  role = aws_iam_role.ec2_ssm_role.name
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-ec2-ssm-profile"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+# ------------------------
+# role policy
+# ------------------------
+resource "aws_iam_role_policy_attachment" "ec2_ssm_profile" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
